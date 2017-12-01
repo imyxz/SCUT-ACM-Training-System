08bb60be910c8b6bbb1c4c27b88d54839fb2f563
imyxz <i@yxz.me>
2017-12-01 23:51:12 +0800

    解耦榜单组件

--- frontend/src/components/Contest/ContestBoard.vue
+++ frontend/src/components/Contest/ContestBoard.vue
@@ -5,43 +5,13 @@
         <input type='checkbox' id='onlyShowInSystem' v-model='onlyShowInSystem' />
         <label for='onlyShowInSystem'>仅显示院队</label>
       </div>
-      <table class='highlight bordered medium-text'>
-        <thead>
-          <tr>
-            <th class='center-align'>Rank</th>
-            <th class='left-align'>Team</th>
-            <th class='center-align'>Solve</th>
-            <th class='center-align'>Penalty</th>
-            <th class='center-align' v-for='x in problemCount' :key='x'>{{ String.fromCharCode(x+64) }}</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr v-for='team in contestBoard' :class="{'blue lighten-5': team.ac_info.in_system}" v-show='!onlyShowInSystem || team.ac_info.in_system' :key='team.name'>
-            <td class='center-align'>{{ team.rank_index }}</td>
-            <td class='left-align'>{{ team.group_name }}</td>
-            <td class='center-align'>{{ team.problem_solved }}</td>
-            <td class='center-align'>{{ team.penalty }}</td>
-            <td v-for='(status,index) in team.ac_info.submission' :class="{
-                            'green':status.ac,
-                            'red':!status.ac && status.is_try}" class='center-align lighten-4' :key='index'>
-              <span v-if='status.ac' style='display: block'>
-                {{ getAcTime(status.ac_time)}}
-              </span>
-              <span v-if='status.try!=0' style='display: block'>
-                (-{{ status.try }})
-              </span>
-              <span style='display: block;height: 0;min-width: 60px;'>
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <contest-board-table :contest-board-info="rank" :problem-count="problemCount"></contest-board-table>
     </div>
   </div>
 </template>
 
 <script>
+import ContestBoardTable from '@/components/Contest/ContestBoardTable'
 export default {
   name: 'ContestBoard',
   props: ['contestBoard', 'problemCount'],
@@ -52,26 +22,35 @@ export default {
   },
   created: function () {
   },
-  methods: {
-    getAcTime: function (sec) {
-      let second, minute, hour
-      second = sec % 60
-      sec /= 60
-      sec = Math.floor(sec)
-      minute = sec % 60
-      sec /= 60
-      sec = Math.floor(sec)
-      hour = sec
-      return this.addZero(hour) + ':' + this.addZero(minute) + ':' + this.addZero(second)
-    },
-    addZero: function (val) {
-      if (val === 0) {
-        return '00'
-      } else if (val < 10) {
-        return '0' + val.toString()
-      } else {
-        return val.toString()
-      }
+  components: {
+    'contest-board-table': ContestBoardTable
+  },
+  computed: {
+    rank: function () {
+      return this.contestBoard
+        .filter(item => {
+          if (this.onlyShowInSystem === false) {
+            return true
+          }
+          return item.ac_info.in_system === true
+        })
+        .map(item => {
+          return {
+            rank_index: item.rank_index,
+            group_name: item.group_name,
+            problem_solved: item.problem_solved,
+            penalty: item.penalty,
+            problems: Object.values(item.ac_info.submission).map(tem => {
+              return {
+                ac: tem.ac,
+                is_try: tem.is_try,
+                ac_time: tem.ac_time,
+                try: tem.try
+              }
+            }),
+            highlight: item.ac_info.in_system === true
+          }
+        })
     }
   }
 }
